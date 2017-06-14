2d7b6ccd3580c8697f5674bbe23303cd9f507f39
Pedro Neto <pjsn@cin.ufpe.br>
2017-06-14 16:40:02 -0300

    Add manipulacao do Fragment sendo demonstrado por um Toast

--- app/src/main/java/compadres/burgueria/app/compadres/CardapioFragment.java
+++ app/src/main/java/compadres/burgueria/app/compadres/CardapioFragment.java
@@ -2,6 +2,7 @@ package compadres.burgueria.app.compadres;
 
 
 import android.os.Bundle;
+import android.support.annotation.IdRes;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
@@ -9,15 +10,15 @@ import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class CardapioFragment extends Fragment {
-
-    private TextView mTextMessage;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -26,10 +27,12 @@ public class CardapioFragment extends Fragment {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_hamburgueres:
-                    mTextMessage.setText(R.string.title_hamburgueres);
+                    Toast.makeText(getActivity().getApplicationContext(),
+                            "Hamburgueres", Toast.LENGTH_LONG).show();
                     return true;
                 case R.id.navigation_bebida:
-                    mTextMessage.setText(R.string.title_bebida);
+                    Toast.makeText(getActivity().getApplicationContext(),
+                            "Bebidas", Toast.LENGTH_LONG).show();
                     return true;
             }
             return false;
@@ -45,8 +48,18 @@ public class CardapioFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_cardapio, container, false);
+        View view = inflater.inflate(R.layout.fragment_cardapio, container, false);
+        // linkar com o componente da view
+        final BottomNavigationView navigation = (BottomNavigationView) view.findViewById(R.id.navigation);
+        navigation.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                // o contexto é obtido de forma diferente
+                navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+            }
+        });
+
+        return view;
     }
 
 }
