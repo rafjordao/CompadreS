54a8a3a14d0a12172d88589e2e4e34c0add69044
pedrojsn96 <pjsn@cin.ufpe.br>
2017-07-01 11:31:19 -0300

    Add botão flutuante

--- app/src/main/java/compadres/burgueria/app/compadres/MainActivity.java
+++ app/src/main/java/compadres/burgueria/app/compadres/MainActivity.java
@@ -93,14 +93,14 @@ public class MainActivity extends AppCompatActivity
         Firebase.setAndroidContext(this);
         mAuth = FirebaseAuth.getInstance();
         mRef=new Firebase("https://compadres-26673.firebaseio.com/");
-        /*FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.car);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
-        });*/
+        });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
